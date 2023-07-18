7ede4715c478d65c9bcd6dcd1c73af2354db7c9e
harshal <harshalp2120@gmail.com>
2023-07-18 14:00:56 +0530

    -Updated MainActivity to launch picker

--- app/src/main/java/com/picker/chucklechooser/MainActivity.kt
+++ app/src/main/java/com/picker/chucklechooser/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val picker = ChucklePicker(
+            context = this,
+            disableSwipeGesture = true,
+            showGallery = true,
+            showFiles = true
+        )
+        val intent = picker.createIntent()
+        startActivity(intent)
+        finish()
     }
 }
